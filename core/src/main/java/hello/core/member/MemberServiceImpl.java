d954ce8f33ce9b6b87dc1ab685fa2e0517646f5b
준모 강 <boldfaced7@hanyang.ac.kr>
2023-05-21 13:20:49 +0900

    AppConfig에서 생성된 객체를 주입 받을 수 있도록 MemberServiceImpl과 OrderServiceImpl에 생성자를 작성

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,10 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
     // 회원 가입/조회를 수행하기 위해 필요한 MemoryMemberRepository 인스턴스를 생성
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Override
     public void join(Member member) {
         memberRepository.save(member);
